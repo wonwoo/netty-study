c9edd6370d0b73ba0a733159c6b032b1e5a299d5
wonwoo <aoruqjfu@gmail.com>
2016-03-27 19:50:55 +0900

    netty chat server

--- src/main/java/me/wonwoo/chat/ChatNettyMessageCodec.java
+++ src/main/java/me/wonwoo/chat/ChatNettyMessageCodec.java
@@ -1,19 +1,16 @@
 package me.wonwoo.chat;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageCodec;
+import io.netty.handler.codec.MessageToMessageDecoder;
 
 import java.util.List;
 
 /**
  * Created by wonwoo on 2016. 3. 27..
  */
-public class ChatNettyMessageCodec extends MessageToMessageCodec<String, Message> {
+public class ChatNettyMessageCodec extends MessageToMessageDecoder<String> {
 
-  protected void encode(ChannelHandlerContext ctx, Message msg, List<Object> out) throws Exception {
-    out.add(msg.toString() + "\n");
-  }
-
+  @Override
   protected void decode(ChannelHandlerContext ctx, String msg, List<Object> out) throws Exception {
     String command = msg.substring(0, 2);
     String message = msg.substring(2, msg.length() - 1) + "\n";
